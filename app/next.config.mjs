3c73aacfe1e77dfbbe57386d458c9e28aa0c8c38
bigbrainbrian7 <brianhyu08@gmail.com>
2026-08-23 17:44:50 -0700

    small bug

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/dashboard-api/:path*',
-        destination: `${apiOrigin}/api/:path*`,
+        destination: `${apiOrigin}/:path*`,
       },
     ]
   },
